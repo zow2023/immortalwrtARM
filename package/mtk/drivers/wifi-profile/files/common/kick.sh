7a7695759f60a74f7110181fb82053380075afc2
thl <meisreallyba@gmail.com>
2023-04-13 14:27:08 +0800

    sync with mt7981

---
+++ package/mtk/drivers/wifi-profile/files/common/kick.sh
@@ -0,0 +1,16 @@
+#!/bin/sh /etc/rc.common
+
+start() {
+    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	iwpriv ra0 set "$kick"
+    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b1.dat)
+    	iwpriv rai0 set "$kick"
+    	kick=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	iwpriv ra1 set "$kick"
+    	kick=${kick/KickStaRssiHigh/AssocReqRssiThres}
+    	iwpriv ra0 set "$kick"
+    	
+}
+
+
+
