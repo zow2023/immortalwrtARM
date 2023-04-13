42922df52070781a19f83608fd68c6a4f6934122
padavanonly <83120842+padavanonly@users.noreply.github.com>
2023-04-13 14:43:17 +0800

    Update kick.sh

--- package/mtk/drivers/wifi-profile/files/common/kick.sh
+++ package/mtk/drivers/wifi-profile/files/common/kick.sh
@@ -1,15 +1,12 @@
 #!/bin/sh /etc/rc.common
 
 start() {
-    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7986-ax6000.dbdc.b0.dat)
     	iwpriv ra0 set "$kick"
-    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b1.dat)
+    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7986-ax6000.dbdc.b1.dat)
     	iwpriv rai0 set "$kick"
-    	kick=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
-    	iwpriv ra1 set "$kick"
-    	kick=${kick/KickStaRssiHigh/AssocReqRssiThres}
-    	iwpriv ra0 set "$kick"
-    	
+    	kick=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7986-ax6000.dbdc.b0.dat)
+    	iwpriv ra1 set "$kick"    	
 }
 
 
