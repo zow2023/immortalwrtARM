bc1ed8d2adaae88b0356ec048323d03be8cb40d4
thl <meisreallyba@gmail.com>
2023-04-16 16:36:23 +0800

    add hwqos support

--- package/mtk/drivers/wifi-profile/files/common/kick.sh
+++ package/mtk/drivers/wifi-profile/files/common/kick.sh
@@ -1,12 +1,29 @@
 #!/bin/sh /etc/rc.common
 
 start() {
-    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7986-ax6000.dbdc.b0.dat)
+    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
     	iwpriv ra0 set "$kick"
-    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7986-ax6000.dbdc.b1.dat)
+    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b1.dat)
     	iwpriv rai0 set "$kick"
-    	kick=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7986-ax6000.dbdc.b0.dat)
-    	iwpriv ra1 set "$kick"    	
+    	kick=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	iwpriv ra1 set "$kick"
+    	ApCliBridge=$(grep -e "ApCliBridge=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	ApCliBridge=${ApCliBridge//ApCliBridge=/}
+    	if [ $ApCliBridge -eq 0 ]; then
+    	brctl delif br-lan apcli0
+    	else
+    	ifconfig apcli0 up
+    	brctl addif br-lan apcli0
+    	fi
+    	
+    	ApCliBridge=$(grep -e "ApCliBridge=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	ApCliBridge=${ApCliBridge//ApCliBridge=/}
+    	if [ $ApCliBridge -eq 0 ]; then
+    	brctl delif br-lan apclii0
+    	else
+    	ifconfig apclii0 up
+    	brctl addif br-lan apclii0
+    	fi
 }
 
 
