b25a27678ed70771b22743a3296ee3bedceba2d8
padavanonly <83120842+padavanonly@users.noreply.github.com>
2023-04-19 10:10:36 +0800

    Update kick.sh

--- package/mtk/drivers/wifi-profile/files/common/kick.sh
+++ package/mtk/drivers/wifi-profile/files/common/kick.sh
@@ -9,20 +9,20 @@ start() {
     	iwpriv ra1 set "$kick"
     	ApCliBridge=$(grep -e "ApCliBridge=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
     	ApCliBridge=${ApCliBridge//ApCliBridge=/}
-    	if [ $ApCliBridge -eq 0 ]; then
-    	brctl delif br-lan apcli0
+    	if [ $ApCliBridge -eq 1 ]; then
+    	brctl addif br-lan apcli0
     	else
     	ifconfig apcli0 up
-    	brctl addif br-lan apcli0
+    	brctl delif br-lan apcli0
     	fi
     	
     	ApCliBridge=$(grep -e "ApCliBridge=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
     	ApCliBridge=${ApCliBridge//ApCliBridge=/}
-    	if [ $ApCliBridge -eq 0 ]; then
-    	brctl delif br-lan apclii0
+    	if [ $ApCliBridge -eq 1 ]; then
+    	brctl addif br-lan apclii0
     	else
     	ifconfig apclii0 up
-    	brctl addif br-lan apclii0
+    	brctl delif br-lan apclii0
     	fi
 }
 
