037782cae960e91498d86071d30bc4a9295ee75f
thl <meisreallyba@gmail.com>
2023-04-21 15:37:02 +0800

    disable mtkiappd by default

--- package/mtk/drivers/wifi-profile/files/common/kick.sh
+++ package/mtk/drivers/wifi-profile/files/common/kick.sh
@@ -5,8 +5,8 @@ start() {
     	iwpriv ra0 set "$kick"
     	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mediatek/mt7981.dbdc.b1.dat)
     	iwpriv rai0 set "$kick"
-    	kick=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
-    	iwpriv ra1 set "$kick"
+    	kick1=$(grep -e "KickStaRssiHigh=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
+    	iwpriv ra1 set "$kick1"
     	ApCliBridge=$(grep -e "ApCliBridge=" /etc/wireless/mediatek/mt7981.dbdc.b0.dat)
     	ApCliBridge=${ApCliBridge//ApCliBridge=/}
     	if [ $ApCliBridge -eq 1 ]; then
@@ -24,6 +24,13 @@ start() {
     	ifconfig apclii0 up
     	brctl delif br-lan apclii0
     	fi
+    	FtSupport=$(grep -e "FtSupport=" /etc/wireless/mediatek/mt7981.dbdc.b1.dat)
+    	FtSupport=${FtSupport//FtSupport=/}
+    	if [ $FtSupport -eq 1 ]; then
+    	/usr/sbin/mtkiappd -e br-lan -wi rai0 -wi ra0 &
+    	else
+    	killall mtkiappd
+    	fi 	
 }
 
 
