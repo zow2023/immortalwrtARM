f5fe24ee79600d432d1357a5e689618a76187e31
thunder361 <73006038+thunder361@users.noreply.github.com>
2023-04-28 14:07:17 +0800

    Add files via upload

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -289,3 +289,20 @@ define Device/xiaomi_wr30u-112M
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += xiaomi_wr30u-112M
+
+define Device/mt7981-komi-a31
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := KOMI A31
+  DEVICE_DTS := mt7981-komi-a31
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := komi,a31
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7981-komi-a31
