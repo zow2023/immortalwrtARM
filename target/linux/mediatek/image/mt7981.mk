e68aa3650a4b3a79be4ca99b6229cf3681b6ad11
thl <meisreallyba@gmail.com>
2023-05-30 10:40:30 +0800

    add h3c 112m uboot support

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -273,6 +273,23 @@ define Device/h3c_nx30pro
 endef
 TARGET_DEVICES += h3c_nx30pro
 
+define Device/h3c_nx30pro-112M
+  DEVICE_VENDOR := H3C
+  DEVICE_MODEL := NX30PRO(with 112M ubi)
+  DEVICE_DTS := mt7981-h3c-nx30pro-112M
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := h3c,nx30pro
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += h3c_nx30pro-112M
+
 define Device/xiaomi_wr30u
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := WR30U
